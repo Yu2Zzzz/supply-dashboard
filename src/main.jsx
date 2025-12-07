0465a10c0447b6dc9128c23a974ee962a3f32cb6
Howard Zheng <howardzheng04@gmail.com>
2025-12-07 04:46:57 -0500

    auto update frontend

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,10 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import './index.css';
-import App from './App.jsx';
-
-// ⬇⬇⬇ 添加这行：从 contexts 文件夹导入 AuthProvider
-import { AuthProvider } from './contexts/AuthContext';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    {/* ⬇⬇⬇ 用 AuthProvider 包住 App */}
-    <AuthProvider>
-      <App />
-    </AuthProvider>
-  </StrictMode>
-);
+    <App />
+  </StrictMode>,
+)
